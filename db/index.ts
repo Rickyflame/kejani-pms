17334ba178f9b20cb6021ecdffb1575924cb6058
Rickyflame <kimumuricky@gmail.com>
2026-04-30 14:19:52 +0300

    config Better Auth, Drizzle ORM client, and dev env

---
+++ db/index.ts
@@ -0,0 +1,18 @@
+//db/index.ts
+
+import {drizzle} from 'drizzle-orm/node-postgres'
+import postgres from 'postgres';
+
+//Connection string comes from .env
+//Drizzle connects via supabase transaticion pooler port 6543
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+//Disable prefetch as its not used in transaction pooler
+const client = postgres(connectionString, {prepare: false});
+
+export const db = drizzle(client);
---
+++ drizzle.dev.config.ts
@@ -0,0 +1,14 @@
+//drizzle.dev.config.ts
+import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local'});
+
+export default {
+    schema: './db/schema.ts',
+    out: './db/migrations',
+    dialect: 'postgresql',
+    dbCredentials:{
+        url: process.env.DATABASE_URL!,
+    },
+} satisfies Config;
