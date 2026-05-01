b4f54207e791a3feda97089317679aab5b00bef7
Rickyflame <kimumuricky@gmail.com>
2026-05-01 07:37:48 +0300

    stag n prod DB up pg_stat verified W1D6

---
+++ drizzle.prod.config.ts
@@ -0,0 +1,15 @@
+// drizzle.prod.config.ts
+
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env.production" });
+
+export default {
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+} satisfies Config;
---
+++ drizzle.staging.config.ts
@@ -0,0 +1,15 @@
+// drizzle.staging.config.ts
+
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env.staging" });
+
+export default {
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+} satisfies Config;
